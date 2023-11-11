e0732cc603cced53d9a0a3ff3c75dd2abad8f8f1
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-11 14:15:33 -0300

    move EmailValidator to file

--- src/utils/email.validator.spec.js
+++ src/utils/email.validator.spec.js
@@ -1,20 +1,26 @@
+const EmailValidator = require('../utils/email-validator')
 const validator = require('validator')
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
+const makeSut = () => {
+  return new EmailValidator()
 }
 describe('', () => {
   test('Should return true if validators true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
 
   test('Should return true if validators false', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     validator.isEmailValid = false
     const isEmailValid = sut.isValid('invalid_email@gmail.com')
     expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validator with correct email', () => {
+    const sut = makeSut()
+    const anyEmail = 'any_email@gmail.com'
+    sut.isValid(anyEmail)
+    expect(validator.email).toBe(anyEmail)
   })
 })
