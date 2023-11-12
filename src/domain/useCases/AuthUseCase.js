161d6cad3c1c299b0eebc9bfe55b38bcf9a4f7ef
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-12 11:40:12 -0300

    simplify code

--- src/domain/useCases/AuthUseCase.js
+++ src/domain/useCases/AuthUseCase.js
@@ -1,19 +1,25 @@
 const { MissingParamError } = require('../../utils/errors')
 module.exports = class AuthUseCase {
-  constructor (loadUserByEmailRepository, encrypter, tokenGenerator) {
+  constructor(loadUserByEmailRepository, encrypter, tokenGenerator) {
     this.loadUserByEmailRepository = loadUserByEmailRepository
     this.encrypter = encrypter
     this.tokenGenerator = tokenGenerator
   }
 
   async auth (email, password) {
-    if (!email) throw new MissingParamError('email')
-    if (!password) throw new MissingParamError('password')
+    if (!email) {
+      throw new MissingParamError('email')
+    }
+    if (!password) {
+      throw new MissingParamError('password')
+    }
     const user = await this.loadUserByEmailRepository.load(email)
-    if (!user) return null
-    const isValid = await this.encrypter.compare(password, user.password)
-    if (!isValid) return null
-    const token = await this.tokenGenerator.generate(user.id)
-    return token
-  }
+    const isValid = user && await this.encrypter.compare(password, user.password)
+    if (user && isValid) {
+      const token = await this.tokenGenerator.generate(user.id)
+      return token
+    }
+
+    return null
+}
 }
