bdc8c4ec646e18a8e53b2efb8cec9d954e3d3d31
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-15 14:08:57 -0300

    encryptor compare method returns false with encrypter returns false and mock lib bcrypt

---
+++ __mocks__/bcrypt.js
@@ -0,0 +1,6 @@
+module.exports = {
+  isValid: true,
+  async compare (value, anyHash) {
+    return this.isValid
+  }
+}
--- src/utils/encrypter.spec.js
+++ src/utils/encrypter.spec.js
@@ -1,12 +1,22 @@
+const bcrypt = require('bcrypt')
+
 class Encrypter {
-  async compare (password, hashPassword) {
-    return true
+  async compare (value, hash) {
+    const isValid = await bcrypt.compare(value, hash)
+    return isValid
   }
 }
 describe('Encrypter', () => {
   test('Should return true if bcrypt if bcrypt returns true', async () => {
     const sut = new Encrypter()
-    const isValid = await sut.compare('any_password', 'hash password')
+    const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBe(true)
+  })
+
+  test('Should return false if bcrypt if bcrypt returns false', async () => {
+    const sut = new Encrypter()
+    bcrypt.isValid = false
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
   })
 })
