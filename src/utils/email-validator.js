7d21e3a9bd383ac6952117ed65252b725963db68
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-15 14:35:31 -0300

    Should throw if no email are provided on Email validator and change to async method isValid

--- src/utils/email-validator.js
+++ src/utils/email-validator.js
@@ -1,6 +1,8 @@
 const validator = require('validator')
+const { MissingParamError } = require('./errors')
 class EmailValidator {
-  isValid (email) {
+  async isValid (email) {
+    if (!email) throw new MissingParamError('email')
     return validator.isEmail(email)
   }
 }
--- src/utils/email.validator.spec.js
+++ src/utils/email.validator.spec.js
@@ -1,19 +1,20 @@
 const EmailValidator = require('../utils/email-validator')
 const validator = require('validator')
+const { MissingParamError } = require('./errors')
 const makeSut = () => {
   return new EmailValidator()
 }
 describe('', () => {
-  test('Should return true if validators true', () => {
+  test('Should return true if validators true', async () => {
     const sut = makeSut()
-    const isEmailValid = sut.isValid('valid_email@gmail.com')
+    const isEmailValid = await sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
 
-  test('Should return true if validators false', () => {
+  test('Should return false if validators false', async () => {
     const sut = makeSut()
     validator.isEmailValid = false
-    const isEmailValid = sut.isValid('invalid_email@gmail.com')
+    const isEmailValid = await sut.isValid('invalid_email@gmail.com')
     expect(isEmailValid).toBe(false)
   })
 
@@ -22,5 +23,10 @@ describe('', () => {
     const anyEmail = 'any_email@gmail.com'
     sut.isValid(anyEmail)
     expect(validator.email).toBe(anyEmail)
+  })
+
+  test('Should throw if no email are provided', async () => {
+    const sut = makeSut()
+    expect(sut.isValid).rejects.toThrow(new MissingParamError('email'))
   })
 })
