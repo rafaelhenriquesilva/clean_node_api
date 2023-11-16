2f6eeb76f5a6ec0b8e59e77a143ac829e6605a9a
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-16 16:41:16 -0300

    create class connect mongo with mongoose

---
+++ src/infra/repositories/database.js
@@ -0,0 +1,26 @@
+// database.js
+const mongoose = require('mongoose')
+
+class Database {
+  async connect (dbName) {
+    const url = 'mongodb://root:example@localhost:27017/?authMechanism=DEFAULT'
+    const options = {
+      user: 'root',
+      pass: 'example',
+      dbName
+    }
+
+    mongoose.connect(url, options)
+
+    this.db = mongoose.connection
+
+    this.db.on('error', console.error.bind(console, 'Erro de conexão com o MongoDB:'))
+    this.db.once('open', () => {
+      console.log('Conexão bem-sucedida com o MongoDB!')
+    })
+
+    return this.db
+  }
+}
+
+module.exports = Database
