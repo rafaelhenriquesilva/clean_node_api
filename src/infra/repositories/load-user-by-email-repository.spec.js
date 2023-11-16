960655c262a56bb5e00892fee6e8c88b55be80ea
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-11-16 16:45:06 -0300

    test all classes repositories

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,13 +1,37 @@
-class LoadUserByEmailRepository {
-  load (email) {
-    return null
-  }
-}
-
+const UserModel = require('./user.model')
+const LoadUserByEmailRepository = require('./load-user-by-email-repository')
+const UserRepository = require('./user-repository')
+const Database = require('./database')
 describe('LoadUserByEmail Repository', () => {
-  test('Should return null if no user is found', async () => {
-    const sut = new LoadUserByEmailRepository()
-    const user = await sut.load('invalid_email@mail.com')
-    expect(user).toBeNull()
+  let database, connection
+  beforeAll(async () => {
+    database = new Database()
+    connection = await database.connect('local')
   })
+
+  beforeEach(async () => {
+    const userRepository = new UserRepository(UserModel)
+    await userRepository.deleteAllUsers()
+  })
+
+  afterAll(async () => {
+    await connection.close()
+  })
+
+  test('Should return null if no user is found', async () => {
+    const userRepository = new UserRepository(UserModel)
+    const loadUserRepository = new LoadUserByEmailRepository(userRepository)
+
+    const user = await loadUserRepository.load('invalid_email@mail.com')
+    expect(user).toEqual([])
+  }, 15000)
+
+  test('Should return email if user is found', async () => {
+    const userRepository = new UserRepository(UserModel)
+    const loadUserRepository = new LoadUserByEmailRepository(userRepository)
+    await userRepository.insertUser('alice@example.com')
+
+    const user = await loadUserRepository.load('alice@example.com')
+    expect(user[0].email).toBe('alice@example.com')
+  }, 15000)
 })
