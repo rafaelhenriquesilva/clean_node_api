75ecd791933e91877facaa2438dbc5af16371406
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-12-03 14:04:14 -0300

    connect to mongo before listen app

--- src/main/index.js
+++ src/main/index.js
@@ -1,2 +1,8 @@
-const app = require('./config/app')
-app.listen(3000, () => console.info('Server running'))
+const Database = require('../infra/config/database')
+const database = new Database()
+database.connect('local')
+  .then(() => {
+    const app = require('./config/app')
+    app.listen(3000, () => console.info('Server running'))
+  })
+  .catch(console.error)
