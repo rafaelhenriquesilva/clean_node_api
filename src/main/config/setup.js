646e5b917edb2f0a3b5de2303618707f7080f8c7
Rafael Candido <rafaelhenriquesilva494@gmail.com>
2023-12-03 12:29:11 -0300

    move test cors and config complete setup

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,5 +1,9 @@
 const corsMid = require('../middlewares/cors')
+const bodyParserMid = require('../middlewares/json-parser')
+const contentTypeMid = require('../middlewares/content-type')
 module.exports = (app) => {
   app.disable('x-powered-by')
   app.use(corsMid)
+  bodyParserMid(app)
+  app.use(contentTypeMid)
 }
